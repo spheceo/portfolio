d9f9a722e91c77dbb1a79c3cbadf164964e2cefc
S-P-H-E <sphe.g.personal@gmail.com>
2026-02-13 11:38:22 +0200

    added socrates background image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
+  
   experimental: {
     fonts: [{
       provider: fontProviders.google(),
       name: "Geist Mono",
-      cssVariable: "--font-geist-mono",
+      cssVariable: "--font-site",
     }]
   },
 
